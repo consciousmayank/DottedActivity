4baf0bf2374b6403b11f2f9bdebbb6bb587a8813
Mayank <mayank123>
2016-05-09 19:51:52 +0530

    signature

--- app/src/main/java/com/mayank/dottedactivity/MainActivity.java
+++ app/src/main/java/com/mayank/dottedactivity/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+/**
+ * Created by Mayank Joshi on 9/5/16.
+ */
 public class MainActivity extends AppCompatActivity {
 
     Button startButton, hideButton;
